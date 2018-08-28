dbcef3bd96c25227e64d2f79557963087743777b
Will <will.lucen4@gmail.com>
2018-08-27 23:42:21 -0300

    Criado mapa 2d colorido e com arvores

---
+++ Assets/Scripts/MapDisplay.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapDisplay : MonoBehaviour
+{
+    public Renderer textureRender;
+
+    public void drawTexture(Texture2D texture)
+    {
+        textureRender.sharedMaterial.mainTexture = texture;
+        textureRender.transform.localScale = new Vector3(texture.width, texture.height, 1);
+    }
+}
---
+++ Assets/Scripts/MapGenerator.cs
@@ -0,0 +1,104 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MapGenerator : MonoBehaviour
+{
+    public enum DrawMode
+    {
+        Noise,
+        Color
+    }
+
+    public DrawMode drawMode;
+    public int mapWidth;
+    public int mapHeight;
+    public float noiseScale;
+    public int octaves;
+    [Range(0, 1)]
+    public float persistance;
+    public float lacunarity;
+    public int seed;
+    public int treeRate;
+    public Vector2 offset;
+
+    public bool autoUpdate;
+    public TerrainType[] regions;
+
+    public void GenerateMap()
+    {
+        float[,] noiseMap = Noise.GenerateNoiseMap(mapWidth, mapHeight, seed, noiseScale, octaves, persistance, lacunarity, offset);
+
+        Color[] colorMap = new Color[mapWidth * mapHeight];
+		for (int y = 0; y < mapHeight; y++) {
+			for (int x = 0; x < mapWidth; x++) {
+				float currentHeight = noiseMap [x, y];
+				for (int i = 0; i < regions.Length; i++) {
+					if (currentHeight <= regions [i].height) {
+						colorMap [y * mapWidth + x] = regions [i].color;
+						break;
+					}
+				}
+                double max = 0;
+                // there are more efficient algorithms than this
+                for (int yn = y - treeRate; yn <= y + treeRate; yn++)
+                {
+                    for (int xn = x - treeRate; xn <= x + treeRate; xn++)
+                    {
+                        if (yn > 0 && yn < mapHeight && xn > 0 && xn < mapWidth)
+                        {
+                            double e = noiseMap[yn, xn];
+                            if (e > max) { max = e; }
+                        }
+                    }
+                }
+                if (noiseMap[y, x] == max && noiseMap[y, x] > regions[0].height)
+                {
+                    colorMap[y * mapWidth + x] = regions[regions.Length-1].color;
+                }
+            }
+		}
+
+        MapDisplay display = FindObjectOfType<MapDisplay>();
+        if (drawMode.Equals(DrawMode.Noise))
+        {
+            display.drawTexture(TextureGenerator.textureFromHeightMap(noiseMap));
+        }
+        else if (drawMode.Equals(DrawMode.Color))
+        {
+            display.drawTexture(TextureGenerator.textureFromColorMap(colorMap, mapWidth, mapHeight));
+        }
+    }
+
+    private void OnValidate()
+    {
+        if (mapWidth < 1)
+        {
+            mapWidth = 1;
+        }
+        if (mapHeight < 1)
+        {
+            mapHeight = 1;
+        }
+        if (lacunarity < 1)
+        {
+            lacunarity = 1;
+        }
+        if (octaves < 0)
+        {
+            octaves = 0;
+        }
+        if (treeRate < 1)
+        {
+            treeRate = 1;
+        }
+    }
+
+}
+
+[System.Serializable]
+public struct TerrainType
+{
+    public string name;
+    public float height;
+    public Color color;
+}
